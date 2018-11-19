defc7868f56637150162b290d68e0619b85b1b7b
Shdoyle <shdoyle@shaw.ca>
2018-11-19 06:33:31 +0000

    Test_Script3

--- lab8git.sh
+++ lab8git.sh
@@ -9,5 +9,5 @@ git commit -m $1
 echo -e "Finished Commit\n"
 
 echo -e "\nPushing to Origin"
-git remote add origin https://github.com/Shdoyle/COSC419Lab8.git
+git push -u origin master
 echo -e "Finished Push\n"
